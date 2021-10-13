18fe661a83275a9affc38b8e9472489164876364
Rhuan Lima <rhuan.lima@gmail.com>
2021-10-13 19:37:20 -0300

    mod_3 update

--- MOD03/imoveis.py
+++ MOD03/imoveis.py
@@ -4,33 +4,29 @@ from typing import AsyncIterable
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+import json
+import time
 
 # %%
-url = 'https://www.vivareal.com.br/venda/parana/curitiba/apartamento_residencial/?pagina={}'
+url = "https://glue-api.vivareal.com/v2/listings?addressCity=Curitiba&addressLocationId=BR>Parana>NULL>Curitiba&addressNeighborhood=&addressState=Paraná&addressCountry=Brasil&addressStreet=&addressZone=&addressPointLat=-25.437238&addressPointLon=-49.269973&business=SALE&facets=amenities&unitTypes=APARTMENT&unitSubTypes=UnitSubType_NONE,DUPLEX,LOFT,STUDIO,TRIPLEX&unitTypesV3=APARTMENT&usageTypes=RESIDENTIAL&listingType=USED&parentId=null&categoryPage=RESULT&includeFields=search(result(listings(listing(displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,unitTypes,nonActivationReason,propertyType,unitSubTypes,id,portal,parkingSpaces,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,bedrooms,pricingInfos,showPrice,status,advertiserContact,videoTourLink,whatsappNumber,stamps),account(id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,phones),medias,accountLink,link)),totalCount),page,seasonalCampaigns,fullUriFragments,nearby(search(result(listings(listing(displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,unitTypes,nonActivationReason,propertyType,unitSubTypes,id,portal,parkingSpaces,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,bedrooms,pricingInfos,showPrice,status,advertiserContact,videoTourLink,whatsappNumber,stamps),account(id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,phones),medias,accountLink,link)),totalCount)),expansion(search(result(listings(listing(displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,unitTypes,nonActivationReason,propertyType,unitSubTypes,id,portal,parkingSpaces,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,bedrooms,pricingInfos,showPrice,status,advertiserContact,videoTourLink,whatsappNumber,stamps),account(id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,phones),medias,accountLink,link)),totalCount)),account(id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,phones,phones),developments(search(result(listings(listing(displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,unitTypes,nonActivationReason,propertyType,unitSubTypes,id,portal,parkingSpaces,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,bedrooms,pricingInfos,showPrice,status,advertiserContact,videoTourLink,whatsappNumber,stamps),account(id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,phones),medias,accountLink,link)),totalCount)),owners(search(result(listings(listing(displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,unitTypes,nonActivationReason,propertyType,unitSubTypes,id,portal,parkingSpaces,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,bedrooms,pricingInfos,showPrice,status,advertiserContact,videoTourLink,whatsappNumber,stamps),account(id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,phones),medias,accountLink,link)),totalCount))&size=350&from={}&q=&developmentsSize=5&__vt=&levels=CITY,UNIT_TYPE&ref=/venda/parana/curitiba/apartamento_residencial/&pointRadius="
+
+
+headersList = {
+ "Accept": "*/*",
+ "User-Agent": "Thunder Client (https://www.thunderclient.io)",
+ "x-domain": "www.vivareal.com.br" 
+}
+
+payload = ""
 
 # %%
 
+def get_json(url,i, headersList, payload):
+    ret = requests.request("GET", url.format(0), data=payload,  headers=headersList)
+    soup = bs(ret.text, 'html.parser')
+    return json.loads(soup.text)
 
-i = 1 
-ret = requests.get(url.format(i))
-soup = bs(ret.text)
-
-# %%
-houses = soup.find_all(
-    'a', {'class': 'property-card__content-link js-card-title'})
-qtd_imoveis = float(soup.find('strong', {'class': 'results-summary__count'}).text.replace('.',''))
-# %%
-len(houses)
-# %%
-qtd_imoveis /36
-
-# %%
-house = houses[0]
-
-
-# %%
-house
-# %%
+#%%
 df = pd.DataFrame(
     columns=[
         'descricao',
@@ -44,54 +40,58 @@ df = pd.DataFrame(
         'wlink'
     ]
 )
-i = 0
+
 
 #%%
-while qtd_imoveis > df.shape[0]:
-    i += 1
-    print(f"valor i: {i} \t\t qtd_imoveis: {df.shape[0]}")
-    ret = requests.get(url.format(i))
-    soup = bs(ret.text)
-    houses = soup.find_all(
-        'a', {'class': 'property-card__content-link js-card-title'})
-
-    for house in houses:
-        try:
-            descricao = house.find('span', {'class': 'property-card__title'}).text.strip()
+imovel_id = 0
+json_data = get_json(url,imovel_id, headersList, payload)
+while len(json_data['search']['result']['listings']) > 0:
+    qtd = len(json_data['search']['result']['listings'])
+    print(f'Qtd de imóveis: {qtd}| total{imovel_id}')
+    for i in range(0, qtd):
+        try: 
+            descricao = json_data['search']['result']['listings'][i]['listing']['title']
         except:
-            descricao = None
+            descricao = '-'
         try:
-            endereco = house.find('span', {'class': 'property-card__address'}).text.strip()
+            try:
+                endereco = json_data['search']['result']['listings'][i]['listing']['address']['street'] +", "+json_data['search']['result']['listings'][i]['listing']['address']['streetNumber']
+            except:
+                endereco = json_data['search']['result']['listings'][i]['listing']['address']['street']
         except:
-            endereco = None
+            endereco = '-'
         try:
-            area = house.find('span', {'class': 'js-property-card-detail-area'}).text.strip()
+            area = json_data['search']['result']['listings'][i]['listing']['totalAreas']
         except:
-            area = None
+            area = '-'
         try:
-            quartos = house.find('li', {'class': 'property-card__detail-room'}).span.text.strip()
+            quartos = json_data['search']['result']['listings'][i]['listing']['bedrooms']
         except:
-            quartos = None
+            quartos = '-'
         try:
-            wc = house.find('li', {'class': 'property-card__detail-bathroom'}).span.text.strip()
+            suites = json_data['search']['result']['listings'][i]['listing']['suites']
         except:
-            wc = None
+            suites = '-'
         try:
-            vagas = house.find('li', {'class': 'property-card__detail-garage'}).span.text.strip()
+            wc = json_data['search']['result']['listings'][i]['listing']['bathrooms']
         except:
-            vagas = None
+            wc = '-'
         try:
-            valor = house.find('div', {'class': 'property-card__price'}).p.text.strip()
+            valor = json_data['search']['result']['listings'][i]['listing']['pricingInfos'][0]['price']
         except:
-            valor = None
+            valor = '-'
         try:
-            condominio = house.find('strong', {'class': 'js-condo-price'}).text.strip()
+            vagas = json_data['search']['result']['listings'][i]['listing']['parkingSpaces']
         except:
-            condominio = None
+            vagas = '-'
         try:
-            wlink = 'https://www.vivareal.com.br' + house['href']
+            condominio = json_data['search']['result']['listings'][i]['listing']['pricingInfos'][0]['monthlyCondoFee']
         except:
-            wlink = None
+            condominio = '-'
+        try:
+            wlink = 'https://www.vivareal.com.br/' + json_data['search']['result']['listings'][i]['link']['href']
+        except:
+            wlink = '-'
 
         df.loc[df.shape[0]] = [
             descricao,
@@ -104,26 +104,10 @@ while qtd_imoveis > df.shape[0]:
             condominio,
             wlink
         ]
-
-
-
+    imovel_id = imovel_id + qtd
+    time.sleep(3)
+    json_data = get_json(url,imovel_id, headersList, payload)
 
 #%%
-
-print(descricao)
-print(endereco)
-print(area)
-print(quartos)
-print(wc)
-print(vagas)
-print(valor)
-print(condominio)
-print(wlink)
-#%%
-
-
 df.to_csv('banco_de_imoveis.csv', sep=';', index=False)
-
-
-
 # %%
